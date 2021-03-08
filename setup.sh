1c7fc9c8c7936b0d4e04ad346685cb3c8f00c7a2
bornite <tmkoikee@gmail.com>
2021-03-08 14:22:43 +0900

    :apple: Update setup.sh
    - Add instructions for deploying starship.toml.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+ts=$(date "+%s")
+
+# screen
 if [ -e ~/.screenrc ]; then
-    mv ~/.screenrc ~/.screenrc.org
+    mv ~/.screenrc ~/.screenrc.org.${ts}
 fi
 ln -s screenrc ~/.screenrc
+
+# zsh
+## starship
+mkdir -p ~/.config
+if [ -e ~/.config/starship.toml ]; then
+    mv ~/.config/starship.toml ~/.config/starship.toml.org.${ts}
+fi
+ln -s starship.toml ~/.config/starship.toml
